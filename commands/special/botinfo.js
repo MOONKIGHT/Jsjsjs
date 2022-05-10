afa47624533f9b4c43e6361a8d7545898207c2ee
MOONKIGHT <102457400+MOONKIGHT@users.noreply.github.com>
2022-05-10 10:17:06 +0530

    Update botinfo.js

--- commands/special/botinfo.js
+++ commands/special/botinfo.js
@@ -11,8 +11,6 @@ exports.run = {
 }
 
 let info = () => {
-   return `The purpose of making this bot is as a learning material and is always online 24 hours (maybe), all the features in this bot are 95% scraping. And you can use the Rest API that I made :
-
-1. https://api.indocoder.dev 
-2. https://api.neoxr.eu.org (Downloader Only)`
+   return `The purpose of making this bot is as a learning material and is always online 24 hours (maybe), all the features in this bot are 95% scraping.
+`
 }
